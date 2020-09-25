ebe321d0b81d4ee261a36e6dd8ebbad988560701
Vinicius Soranço <vinicius.soranco@levty.com>
2020-09-25 17:01:26 -0300

    ajustes pwa

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -35,9 +35,22 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta name="theme-color" content="#081A23" />
+          <meta name='application-name' content='Next Study' />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+          <meta name='apple-mobile-web-app-title' content='Next Study' />
+          <meta name='description' content='Best Next Study in the world' />
+          <meta name='format-detection' content='telephone=no' />
+          <meta name='mobile-web-app-capable' content='yes' />
+          <meta name='msapplication-config' content='/static/icons/browserconfig.xml' />
+          <meta name='msapplication-TileColor' content='#2B5797' />
+          <meta name='msapplication-tap-highlight' content='no' />
+          <meta name='theme-color' content='#000000' />
           <link rel="manifest" href="/manifest.json" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet" />
           <link rel="icon" href="/icons/favicon-32x32.png" />
+          <link rel='apple-touch-icon' sizes='180x180' href='/icons/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-32x32.png' />
         </Head>
         <body>
           <Main />
